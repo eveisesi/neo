c22f2a00b2256cacf7440f846a10f14b378ac307
David Douglas <david@onetwentyseven.dev>
2020-04-18 19:12:33 +0000

    Adding Errors to consts

--- const.go
+++ const.go
@@ -1,5 +1,7 @@
 package neo
 
+import "errors"
+
 const ZKILLBOARD_URL = "https://zkillboard.com"
 
 // Intentionally leaving the %s token. This is meant to be run through Sprintf to replace the token with a date
@@ -7,3 +9,8 @@ const ZKILLBOARD_HISTORY_API = "https://zkillboard.com/api/history/%s.json"
 
 const ESI_URL = "https://esi.evetech.net"
 const SSO_URL = "https://login.eveonline.com"
+
+// Errors
+var ErrRedisNil = errors.New("redis: nil")
+var ErrEsiMaxAttempts = errors.New("max attempts exceeded")
+var ErrEsiTypeNotFound = errors.New("type not found")
