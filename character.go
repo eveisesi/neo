10f71e4f5dfa9f522a3c3e45839220c5b4655ae4
David Douglas <david@onetwentyseven.dev>
2020-06-13 23:38:22 +0000

    This should start updating sec status from esi

--- character.go
+++ character.go
@@ -22,6 +22,7 @@ type Character struct {
 	CorporationID    uint64      `boil:"corporation_id" json:"corporation_id"`
 	AllianceID       null.Uint64 `boil:"alliance_id" json:"alliance_id,omitempty"`
 	FactionID        null.Uint64 `boil:"faction_id" json:"faction_id,omitempty"`
+	SecurityStatus   float64     `boil:"security_status" json:"securityStatus"`
 	NotModifiedCount uint        `boil:"not_modified_count" json:"not_modified_count"`
 	UpdatePriority   uint        `boil:"update_priority" json:"update_priority"`
 	Etag             null.String `boil:"etag" json:"etag"`
