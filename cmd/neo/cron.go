6e5cc749d73c10499e0367bf52e909c845de846e
David Douglas <david@onetwentyseven.dev>
2020-06-28 14:54:54 +0000

    Cron is  panicing because core:=app.New() is being called more than once

--- cmd/neo/cron.go
+++ cmd/neo/cron.go
@@ -32,25 +32,10 @@ func cronCommand() cli.Command {
 				cron.WithSeconds(),
 			)
 
-			_, err = c.AddFunc("0 10 11 * * *", marketDataCron)
-			if err != nil {
-				app.Logger.WithError(err).Fatal("failed to initialize marketDataCron")
-			}
-
-			_, err = c.AddFunc("*/30 * * * * *", esiServerStatusCron)
-			if err != nil {
-				app.Logger.WithError(err).Fatal("failed to initialize esiServerStatusCron")
-			}
-
-			_, err = c.AddFunc("0 * * * * *", trackingJanitorCron)
-			if err != nil {
-				app.Logger.WithError(err).Fatal("failed to initialize trackingJanitorCron")
-			}
-
-			_, err = c.AddFunc("0 0 11 * * *", autocompleteCron)
-			if err != nil {
-				app.Logger.WithError(err).Fatal("failed to initialize autocompleteCron")
-			}
+			registerAutocompleteCron(c, app)
+			registerEsiServerStatusCron(c, app)
+			registerMarketDataCron(c, app)
+			registerTrackingJanitorCron(c, app)
 
 			c.Run()
 
@@ -59,77 +44,96 @@ func cronCommand() cli.Command {
 	}
 }
 
-func autocompleteCron() {
-	app := core.New()
+func registerAutocompleteCron(c *cron.Cron, app *core.App) {
 
-	app.Logger.Info("rebuilding autocompleter index")
+	_, err := c.AddFunc("0 0 11 * * *", func() {
+		app.Logger.Info("rebuilding autocompleter index")
 
-	err := app.Search.Build()
-	if err != nil {
-		app.Logger.WithError(err).Error("failed to rebuild autocompleter index")
-	}
-
-	app.Logger.Info("done rebuilding autocompleter index")
-}
-
-func esiServerStatusCron() {
-	app := core.New()
-
-	app.Logger.Info("checking tq server status")
-
-	serverStatus, m := app.ESI.GetStatus()
-	if m.IsError() {
-		app.Logger.WithError(m.Msg).Error("Failed to fetch tq server status from ESI")
-		return
-	}
-
-	if m.Code != 200 {
-		app.Logger.WithField("code", m.Code).Error("unable to acquire tq server status")
-		return
-	}
-
-	app.Redis.Set(neo.TQ_PLAYER_COUNT, serverStatus.Players, 0)
-	app.Redis.Set(neo.TQ_VIP_MODE, serverStatus.VIP.Bool, 0)
-
-	app.Logger.Info("done checking tq server status")
-
-}
-
-func marketDataCron() {
-	app := core.New()
-
-	app.Logger.Info("starting fetch prices")
-	app.Market.FetchPrices()
-	app.Logger.Info("done with fetch prices")
-	app.Logger.Info("starting fetch history ")
-	app.Market.FetchHistory()
-	app.Logger.Info("done with fetch history ")
-}
-
-func trackingJanitorCron() {
-	app := core.New()
-
-	app.Logger.Info("starting esi tracking set janitor")
-
-	ts := time.Now().Add(time.Minute * -6).UnixNano()
-	sets := []string{
-		neo.REDIS_ESI_TRACKING_OK,
-		neo.REDIS_ESI_TRACKING_NOT_MODIFIED,
-		neo.REDIS_ESI_TRACKING_CALM_DOWN,
-		neo.REDIS_ESI_TRACKING_4XX,
-		neo.REDIS_ESI_TRACKING_5XX,
-	}
-	count := int64(0)
-	for _, set := range sets {
-		a, err := app.Redis.ZRemRangeByScore(set, "-inf", strconv.FormatInt(ts, 10)).Result()
+		err := app.Search.Build()
 		if err != nil {
-			app.Logger.WithError(err).Error("failed to fetch current count of esi success set from redis")
+			app.Logger.WithError(err).Error("failed to rebuild autocompleter index")
+		}
+
+		app.Logger.Info("done rebuilding autocompleter index")
+	})
+	if err != nil {
+		app.Logger.WithError(err).Fatal("failed to initialize autocompleteCron")
+	}
+
+}
+
+func registerEsiServerStatusCron(c *cron.Cron, app *core.App) {
+
+	_, err := c.AddFunc("*/30 * * * * *", func() {
+		app.Logger.Info("checking tq server status")
+
+		serverStatus, m := app.ESI.GetStatus()
+		if m.IsError() {
+			app.Logger.WithError(m.Msg).Error("Failed to fetch tq server status from ESI")
 			return
 		}
-		count += a
+
+		if m.Code != 200 {
+			app.Logger.WithField("code", m.Code).Error("unable to acquire tq server status")
+			return
+		}
+
+		app.Redis.Set(neo.TQ_PLAYER_COUNT, serverStatus.Players, 0)
+		app.Redis.Set(neo.TQ_VIP_MODE, serverStatus.VIP.Bool, 0)
+
+		app.Logger.Info("done checking tq server status")
+
+	})
+	if err != nil {
+		app.Logger.WithError(err).Fatal("failed to initialize esiServerStatusCron")
 	}
 
-	app.Logger.WithField("removed", count).Info("successfully cleared keys from success queue")
-	app.Logger.Info("stopping esi tracking set janitor")
+}
+
+func registerMarketDataCron(c *cron.Cron, app *core.App) {
+
+	_, err := c.AddFunc("0 10 11 * * *", func() {
+		app.Logger.Info("starting fetch prices")
+		app.Market.FetchPrices()
+		app.Logger.Info("done with fetch prices")
+		app.Logger.Info("starting fetch history ")
+		app.Market.FetchHistory()
+		app.Logger.Info("done with fetch history ")
+	})
+	if err != nil {
+		app.Logger.WithError(err).Fatal("failed to initialize marketDataCron")
+	}
+
+}
+
+func registerTrackingJanitorCron(c *cron.Cron, app *core.App) {
+
+	_, err = c.AddFunc("0 * * * * *", func() {
+		app.Logger.Info("starting esi tracking set janitor")
+
+		ts := time.Now().Add(time.Minute * -6).UnixNano()
+		sets := []string{
+			neo.REDIS_ESI_TRACKING_OK,
+			neo.REDIS_ESI_TRACKING_NOT_MODIFIED,
+			neo.REDIS_ESI_TRACKING_CALM_DOWN,
+			neo.REDIS_ESI_TRACKING_4XX,
+			neo.REDIS_ESI_TRACKING_5XX,
+		}
+		count := int64(0)
+		for _, set := range sets {
+			a, err := app.Redis.ZRemRangeByScore(set, "-inf", strconv.FormatInt(ts, 10)).Result()
+			if err != nil {
+				app.Logger.WithError(err).Error("failed to fetch current count of esi success set from redis")
+				return
+			}
+			count += a
+		}
+
+		app.Logger.WithField("removed", count).Info("successfully cleared keys from success queue")
+		app.Logger.Info("stopping esi tracking set janitor")
+	})
+	if err != nil {
+		app.Logger.WithError(err).Fatal("failed to initialize trackingJanitorCron")
+	}
 
 }
