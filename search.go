b1ae762cb84658b04152f51f94612082ee36a962
David Douglas <david@onetwentyseven.dev>
2020-07-05 21:31:29 +0000

    Changes to search result model

--- search.go
+++ search.go
@@ -7,8 +7,9 @@ type SearchRepository interface {
 }
 
 type SearchableEntity struct {
-	ID    uint64 `db:"id" json:"id"`
-	Name  string `db:"name" json:"name"`
-	Type  string `db:"type" json:"type"`
-	Image string `db:"image" json:"image"`
+	ID       uint64 `db:"id" json:"id"`
+	Name     string `db:"name" json:"name"`
+	Type     string `db:"type" json:"type"`
+	Image    string `db:"image" json:"image"`
+	Priority int    `db:"priority" json:"-"`
 }
