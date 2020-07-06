1267a3a13ba78a14da917c266c0dc6e5eabeb0a6
David Douglas <david@onetwentyseven.dev>
2020-07-06 02:45:36 +0000

    Moved date type to its own file

--- market.go
+++ market.go
@@ -2,8 +2,6 @@ package neo
 
 import (
 	"context"
-	"database/sql/driver"
-	"encoding/json"
 	"time"
 
 	"github.com/volatiletech/null"
@@ -42,58 +40,4 @@ type MarketGroup struct {
 	Name          string   `json:"name"`
 	Description   string   `json:"description"`
 	Types         []uint64 `json:"types"`
-}
-
-type Date struct{ time.Time }
-
-func (d *Date) UnmarshalJSON(data []byte) error {
-
-	var s string
-	err := json.Unmarshal(data, &s)
-	if err != nil {
-		return nil
-	}
-
-	t, err := time.Parse("2006-01-02", s)
-	if err != nil {
-		return err
-	}
-
-	*d = Date{t}
-
-	return nil
-}
-
-func (d *Date) MarshalJSON() ([]byte, error) {
-
-	return []byte(d.Format("2016-01-02")), nil
-
-}
-
-func (d *Date) Scan(v interface{}) error {
-	if v == nil {
-		*d = Date{time.Now()}
-		return nil
-	}
-
-	switch v := v.(type) {
-	case time.Time:
-		*d = Date{v}
-		return nil
-	case string:
-		t, e := time.Parse("2006-01-02", v)
-		if e != nil {
-			return e
-		}
-
-		*d = Date{t}
-		return nil
-	}
-
-	return nil
-
-}
-
-func (d *Date) Value() (driver.Value, error) {
-	return d.Format("2006-01-02"), nil
 }
