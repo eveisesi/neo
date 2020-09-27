5ad7719eefbba0d437dbb623456f53be72c15d44
David Douglas <david@onetwentyseven.dev>
2020-09-27 23:42:18 +0000

    env declaration fix again for the ui this time

--- frontend/util/const/urls.js
+++ frontend/util/const/urls.js
@@ -1,7 +1,7 @@
 // Urls
 const EVEONLINE_IMAGE = "https://images.evetech.net/";
 
-const API_BASE = process.env.VUE_APP_API_BASE
+const API_BASE = process.env.NUXT_APP_API_BASE
 const API_URL = `http://${API_BASE}/query`
 const WSS_URL = `ws://${API_BASE}/query`
 
