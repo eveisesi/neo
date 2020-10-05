38f16fb3c008239f491d745726e79729dde86873
David Douglas <david@onetwentyseven.dev>
2020-10-05 00:03:11 +0000

    Removing unused scripts

--- backend/scripts/cron/autocomplete.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/snap/bin/docker-compose -f /root/projects/neo/core/cron.yaml up autocomplete
-/snap/bin/docker-compose -f /root/projects/neo/core/cron.yaml rm -s -f autocomplete
--- backend/scripts/cron/janitor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/snap/bin/docker-compose -f /root/projects/neo/core/cron.yaml up janitor
-/snap/bin/docker-compose -f /root/projects/neo/core/cron.yaml rm -s -f janitor
